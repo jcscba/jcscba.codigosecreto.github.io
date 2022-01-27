f0f5c81297cc3f94dae125ef796817d31067351b
jcscba <jcscba@gmail.com>
2022-01-26 22:46:20 -0300

    Primera version funcionando y con disenio

--- script.js
+++ script.js
@@ -1,73 +1,106 @@
 var botonEncriptar = document.querySelector("#btn-encriptar");
 var botonDesencriptar = document.querySelector("#btn-desencriptar");
+var botonCopiar = document.querySelector("#btn-copy");
 
 	botonEncriptar.addEventListener("click",function(event){
-			event.preventDefault();
-			var textoaMostrar = [];
-			var textoIngresado = document.querySelector("#input-texto");
-			var texto = textoIngresado.value;
+		event.preventDefault();
+		var textoIngresado = document.querySelector("#input-texto");
+		var texto = textoIngresado.value;
 
-			for (var i = 0; i<texto.length; i++){
-				if((texto[i] != "a") && (texto[i] != "e") && (texto[i] != "i") && (texto[i] != "o") && (texto[i] != "u") ) {
-					textoaMostrar.push(texto[i]);
-					}
-				else{
-					switch(texto[i]){
-					case 'a':
-						textoaMostrar.push("ai");
-						break;
-					case 'e':	
-						textoaMostrar.push("enter");
-						break;
-					case 'i':
-						textoaMostrar.push("imes");
-						break;
-					case 'o':
-						textoaMostrar.push("ober");
-						break;
-					case 'u':
-						textoaMostrar.push("ufat");
-						break;
-						}}}
+		/*var caracteresValidos = "abcdefghijklmnopqrstuvwxyz ":
+		if(texto.test(caracteresValidos))
 
+
+		texto = texto.toLowerCase();*/
+
+		var textoaMostrar = encriptarTexto (texto);
 		mostrarTexto(textoaMostrar);
+
 	});
 
 	botonDesencriptar.addEventListener("click",function(event){
 		event.preventDefault();
-		var textoaMostrar = [];
 		var textoIngresado = document.querySelector("#input-texto");
 		var texto = textoIngresado.value;
-
-			for (var i = 0; i<texto.length; i++){
-				if((texto[i] != "a") && (texto[i] != "e") && (texto[i] != "i") && (texto[i] != "o") && (texto[i] != "u") ) {
-					textoaMostrar.push(texto[i]);
-					}
-				else{
-					switch(texto[i]){
-						case 'a':
-							textoaMostrar.push("a");
-							i=i+1;
-							break;
-						case 'e':	
-							textoaMostrar.push("e");
-							i=i+4;
-							break;
-						case 'i':
-							textoaMostrar.push("i");
-							i=i+3;
-							break;
-						case 'o':
-							textoaMostrar.push("o");
-							i=i+3;
-							break;
-						case 'u':
-							textoaMostrar.push("u");
-							i=i+3;
-							break;
-							}}}
+		var textoaMostrar = desencriptarTexto(texto);
 		mostrarTexto(textoaMostrar);	
 	});
+
+	botonCopiar.addEventListener("click",function(event){
+		event.preventDefault();
+		var copiarTexto = document.querySelector("#msg");
+		copiarTexto.focus();
+		document.execCommand("selectAll");
+	//	document.execCommand('copy');
+
+	});
+
+	function encriptarTexto (texto) {
+
+		var textoEncriptado	= [];	
+		for (var i = 0; i<texto.length; i++){
+			if((texto[i] != "a") && (texto[i] != "e") && (texto[i] != "i") && (texto[i] != "o") && (texto[i] != "u") ) {
+				textoEncriptado.push(texto[i]);
+			}
+			else{
+				switch(texto[i]){
+					case 'a':
+						textoEncriptado.push("ai");
+						break;
+					case 'e':	
+						textoEncriptado.push("enter");
+						break;
+					case 'i':
+						textoEncriptado.push("imes");
+						break;
+					case 'o':
+						textoEncriptado.push("ober");
+						break;
+					case 'u':
+						textoEncriptado.push("ufat");
+						break;
+				}
+			}
+		}
+
+		return textoEncriptado;
+	}
+
+	function desencriptarTexto(texto){
+
+		var textoDesencriptado = [];
+		for (var i = 0; i<texto.length; i++){
+			if((texto[i] != "a") && (texto[i] != "e") && (texto[i] != "i") && (texto[i] != "o") && (texto[i] != "u") ) {
+				textoDesencriptado.push(texto[i]);
+			}
+			else{
+				switch(texto[i]){
+					case 'a':
+						textoDesencriptado.push("a");
+						i=i+1;
+						break;
+					case 'e':	
+						textoDesencriptado.push("e");
+						i=i+4;
+						break;
+					case 'i':
+						textoDesencriptado.push("i");
+						i=i+3;
+						break;
+					case 'o':
+						textoDesencriptado.push("o");
+						i=i+3;
+						break;
+					case 'u':
+						textoDesencriptado.push("u");
+						i=i+3;
+						break;
+				}
+			}
+		}
+
+		return textoDesencriptado;
+	}
 
 	function mostrarTexto(textoaMostrar){
 			
@@ -75,6 +108,8 @@ var botonDesencriptar = document.querySelector("#btn-desencriptar");
 			mostrarTexto.value = textoaMostrar.join("");
 			
 	}
+
+
 
 	
 	
